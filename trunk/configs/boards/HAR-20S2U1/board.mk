3a0853927ab6d31f7dc84f0ab8bf0138213d76e5
qewwqewq22 <162088039+qewwqewq22@users.noreply.github.com>
2024-04-15 13:32:20 +0800

    Add files via upload

--- trunk/configs/boards/HAR-20S2U1/board.mk
+++ trunk/configs/boards/HAR-20S2U1/board.mk
@@ -1,9 +1,9 @@
 ##################################################################
 # Board PID # Board Name       # PRODUCT # Note
 ##################################################################
-# Q11Pro       # Q11Pro              # MT7621  #
+# 20s2u1       # 20s2u1             # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_Q11Pro -DBOARD_MT7915_DBDC
+CFLAGS += -DBOARD_20s2u1 -DBOARD_MT7915_DBDC
 BOARD_NUM_USB_PORTS=0
 CONFIG_BOARD_RAM_SIZE=256

755dad64dcf7b8f65e908a8cadf0c3e261f07f79
qewwqewq22 <162088039+qewwqewq22@users.noreply.github.com>
2024-04-15 14:22:17 +0800

    Add files via upload

--- trunk/configs/boards/HAR-20S2U1/board.mk
+++ trunk/configs/boards/HAR-20S2U1/board.mk
@@ -1,9 +1,9 @@
 ##################################################################
 # Board PID # Board Name       # PRODUCT # Note
 ##################################################################
-# 20s2u1       # 20s2u1             # MT7621  #
+# 20S2U1       # 20S2U1             # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_20s2u1 -DBOARD_MT7915_DBDC
+CFLAGS += -DBOARD_20S2U1 -DBOARD_MT7915_DBDC
 BOARD_NUM_USB_PORTS=0
 CONFIG_BOARD_RAM_SIZE=256
